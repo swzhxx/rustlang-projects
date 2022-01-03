1df6d49a59d09262ffb6dbfbde5f2c2784852c08
swzhxx <549909896@qq.com>
2022-01-03 09:24:28 +0800

    feat(queryer ): queryer

--- sql/queryer/src/lib.rs
+++ sql/queryer/src/lib.rs
@@ -1,7 +1,79 @@
 mod convert;
 mod dialect;
+mod fetcher;
+mod loader;
+use anyhow::{anyhow, Result};
+use sqlparser::parser::Parser;
+use std::ops::{Deref, DerefMut};
+use tracing::info;
 
+pub use dialect::example_sql;
 pub use dialect::TyrDialect;
+
+use polars::prelude::*;
+
+use crate::loader::detect_content;
+use crate::{convert::Sql, fetcher::retrieve_data};
+
+#[derive(Debug)]
+pub struct DataSet(DataFrame);
+
+impl Deref for DataSet {
+    type Target = DataFrame;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for DataSet {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl DataSet {
+    pub fn to_csv(&self) -> Result<String> {
+        let mut buf = Vec::new();
+        let writer = CsvWriter::new(&mut buf);
+        writer.finish(self)?;
+        Ok(String::from_utf8(buf)?)
+    }
+}
+
+pub async fn query<T: AsRef<str>>(sql: T) -> Result<DataSet> {
+    let ast = Parser::parse_sql(&TyrDialect::default(), sql.as_ref())?;
+    if ast.len() != 1 {
+        return Err(anyhow!("Only support single sql at the moment"));
+    }
+    let sql = &ast[0];
+    let Sql {
+        source,
+        condition,
+        selection,
+        offset,
+        limit,
+        order_by,
+    } = sql.try_into()?;
+    info!("retrieving data from source: {}", source);
+    // 从 source 读入一个 DataSet
+    let ds = detect_content(retrieve_data(source).await?).load()?;
+
+    let mut filtered = match condition {
+        Some(expr) => ds.0.lazy().filter(expr),
+        None => ds.0.lazy(),
+    };
+
+    filtered = order_by
+        .into_iter()
+        .fold(filtered, |acc, (col, desc)| acc.sort(&col, desc));
+
+    if offset.is_some() || limit.is_some() {
+        filtered = filtered.slice(offset.unwrap_or(0), limit.unwrap_or(usize::MAX));
+    }
+
+    Ok(DataSet(filtered.select(selection).collect()?))
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
