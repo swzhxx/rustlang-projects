addcd81d8edc846d08883e5cc36bc158c605cdbd
swzhxx <549909896@qq.com>
2022-01-09 20:26:48 +0800

    feat(thumbor): thumbor

---
+++ thumbor/src/pb/mod.rs
@@ -0,0 +1,109 @@
+use base64::{decode_config, encode_config, URL_SAFE_NO_PAD};
+use photon_rs::transform::SamplingFilter;
+use prost::Message;
+
+mod abi;
+pub use abi::*;
+
+impl ImageSpec {
+    pub fn new(specs: Vec<Spec>) -> Self {
+        Self { specs }
+    }
+}
+
+// 让ImageSpec可以生成一个字符串
+impl From<&ImageSpec> for String {
+    fn from(image_spec: &ImageSpec) -> Self {
+        let data = image_spec.encode_to_vec();
+        encode_config(data, URL_SAFE_NO_PAD)
+    }
+}
+
+// 让ImageSpec可以通过一个字符串创建。比如s.parse().unwrap()
+impl TryFrom<&str> for ImageSpec {
+    type Error = anyhow::Error;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let data = decode_config(value, URL_SAFE_NO_PAD)?;
+        Ok(ImageSpec::decode(&data[..])?)
+    }
+}
+
+impl filter::Filter {
+    pub fn to_str(&self) -> Option<&'static str> {
+        match self {
+            filter::Filter::Unspecified => None,
+            filter::Filter::Oceanic => Some("oceanic"),
+            filter::Filter::Islands => Some("islands"),
+            filter::Filter::Marine => Some("marine"),
+        }
+    }
+}
+
+impl From<resize::SampleFilter> for SamplingFilter {
+    fn from(v: resize::SampleFilter) -> Self {
+        match v {
+            resize::SampleFilter::Undefined => SamplingFilter::Nearest,
+            resize::SampleFilter::Nearest => SamplingFilter::Nearest,
+            resize::SampleFilter::Triangle => SamplingFilter::Triangle,
+            resize::SampleFilter::CatmullRom => SamplingFilter::CatmullRom,
+            resize::SampleFilter::Gaussian => SamplingFilter::Gaussian,
+            resize::SampleFilter::Lanczos3 => SamplingFilter::Lanczos3,
+        }
+    }
+}
+
+// 提供一些辅助函数，让创建一个spec的过程简单一些
+impl Spec {
+    pub fn new_resize_seam_carve(width: u32, height: u32) -> Self {
+        Self {
+            data: Some(spec::Data::Resize(Resize {
+                width,
+                height,
+                rtype: resize::ResizeType::SeamCarve as i32,
+                filter: resize::SampleFilter::Undefined as i32,
+            })),
+        }
+    }
+
+    pub fn new_resize(width: u32, height: u32, filter: resize::SampleFilter) -> Self {
+        Self {
+            data: Some(spec::Data::Resize(Resize {
+                width,
+                height,
+                rtype: resize::ResizeType::Normal as i32,
+                filter: filter as i32,
+            })),
+        }
+    }
+
+    pub fn new_filter(filter: filter::Filter) -> Self {
+        Self {
+            data: Some(spec::Data::Filter(Filter {
+                filter: filter as i32,
+            })),
+        }
+    }
+
+    pub fn new_watermark(x: u32, y: u32) -> Self {
+        Self {
+            data: Some(spec::Data::Watermark(Watermark { x, y })),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::borrow::Borrow;
+    use std::convert::TryInto;
+
+    #[test]
+    fn encoded_spec_could_be_decoded() {
+        let spec1 = Spec::new_resize(600, 600, resize::SampleFilter::CatmullRom);
+        let spec2 = Spec::new_filter(filter::Filter::Marine);
+        let image_spec = ImageSpec::new(vec![spec1, spec2]);
+        let s: String = image_spec.borrow().into();
+        println!("{:?}", s);
+        assert_eq!(image_spec, s.as_str().try_into().unwrap());
+    }
+}
---
+++ thumbor/src/server1.rs
@@ -0,0 +1,38 @@
+use axum::{extract::Path, handler::get, http::StatusCode, Router};
+use percent_encoding::percent_decode_str;
+use serde::Deserialize;
+
+// 引入protobuf生成的代码，暂时不用关心他们
+mod pb;
+use pb::*;
+
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
+}
+
+#[tokio::main]
+async fn main() {
+    // 初始化 tracing
+    tracing_subscriber::fmt::init();
+    // 构建路由
+    let app = Router::new()
+        // `GET /image` 会执行 generate 函数，并把 spec 和 url 传递过去
+        .route("/image/:spec/:url", get(generate));
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap()
+}
+
+async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
+    let url = percent_decode_str(&url).decode_utf8_lossy();
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+    Ok(format!("url : {} \nspec: {:#?}", url, spec))
+}
