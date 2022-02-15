8935f23623aff28091df15dc5d361594ebd122af
swzhxx <549909896@qq.com>
2022-02-15 22:11:43 +0800

    feat(self-parser): Trait

--- self-parser/src/parser.rs
+++ self-parser/src/parser.rs
@@ -5,6 +5,21 @@ fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
     }
 }
 
+type ParseResult<'a, Output> = Result<(&'a str, Output), &'a str>;
+
+trait Parser<'a, Output> {
+    fn parse(&self, input: &'a str) -> ParseResult<'a, Output>;
+}
+
+impl<'a, F, Output> Parser<'a, Output> for F
+where
+    F: Fn(&'a str) -> ParseResult<Output>,
+{
+    fn parse(&self, input: &'a str) -> ParseResult<'a, Output> {
+        self(input)
+    }
+}
+
 pub fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
     move |input| match input.get(0..expected.len()) {
         Some(next) if next == expected => Ok((&input[expected.len()..], ())),
@@ -31,16 +46,13 @@ pub fn identifier(input: &str) -> Result<(&str, String), &str> {
     Ok((&input[next_index..], matched))
 }
 
-pub fn pair<P1, P2, R1, R2>(
-    parser1: P1,
-    parser2: P2,
-) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+fn pair<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, (R1, R2)>
 where
-    P1: Fn(&str) -> Result<(&str, R1), &str>,
-    P2: Fn(&str) -> Result<(&str, R2), &str>,
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
 {
-    move |input| match parser1(input) {
-        Ok((next_input, result1)) => match parser2(next_input) {
+    move |input| match parser1.parse(input) {
+        Ok((next_input, result1)) => match parser2.parse(next_input) {
             Ok((final_input, result2)) => Ok((final_input, (result1, result2))),
             Err(err) => Err(err),
         },
@@ -48,14 +60,15 @@ where
     }
 }
 
-pub fn map<P, F, A, B>(parser: P, map_fn: F) -> impl Fn(&str) -> Result<(&str, B), &str>
+fn map<'a, P, F, A, B>(parser: P, map_fn: F) -> impl Parser<'a, B>
 where
-    P: Fn(&str) -> Result<(&str, A), &str>,
+    P: Parser<'a, A>,
     F: Fn(A) -> B,
 {
-    move |input| match parser(input) {
-        Ok((next_input, result)) => Ok((next_input, map_fn(result))),
-        Err(err) => Err(err),
+    move |input| {
+        parser
+            .parse(input)
+            .map(|(next_input, result)| (next_input, map_fn(result)))
     }
 }
 #[cfg(test)]
@@ -95,9 +108,9 @@ mod test {
         let tag_opener = pair(match_literal("<"), identifier);
         assert_eq!(
             Ok(("/>", ((), "my-first-element".to_string()))),
-            tag_opener("<my-first-element/>")
+            tag_opener.parse("<my-first-element/>")
         );
-        assert_eq!(Err("oops"), tag_opener("oops"));
-        assert_eq!(Err("!oops"), tag_opener("<!oops"));
+        assert_eq!(Err("oops"), tag_opener.parse("oops"));
+        assert_eq!(Err("!oops"), tag_opener.parse("<!oops"));
     }
 }
