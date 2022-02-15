d2534b76b382bb4d3a3a2e31e18b0b05764f8110
swzhxx <549909896@qq.com>
2022-02-15 21:16:46 +0800

    feat(Combinator): Combinator

--- self-parser/src/parser.rs
+++ self-parser/src/parser.rs
@@ -31,6 +31,22 @@ pub fn identifier(input: &str) -> Result<(&str, String), &str> {
     Ok((&input[next_index..], matched))
 }
 
+pub fn pair<P1, P2, R1, R2>(
+    parser1: P1,
+    parser2: P2,
+) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+where
+    P1: Fn(&str) -> Result<(&str, R1), &str>,
+    P2: Fn(&str) -> Result<(&str, R2), &str>,
+{
+    move |input| match parser1(input) {
+        Ok((next_input, result1)) => match parser2(next_input) {
+            Ok((final_input, result2)) => Ok((final_input, (result1, result2))),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
+}
 #[cfg(test)]
 mod test {
     // use core::num::dec2flt::parse;
@@ -61,5 +77,16 @@ mod test {
             Err("!not at all an identifier"),
             identifier("!not at all an identifier")
         );
+    }
+
+    #[test]
+    fn pair_combinator() {
+        let tag_opener = pair(match_literal("<"), identifier);
+        assert_eq!(
+            Ok(("/>", ((), "my-first-element".to_string()))),
+            tag_opener("<my-first-element/>")
+        );
+        assert_eq!(Err("oops"), tag_opener("oops"));
+        assert_eq!(Err("!oops"), tag_opener("<!oops"));
     }
 }
