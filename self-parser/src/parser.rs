1563c570a51f121d6ecb90edf8b03a1316f694ed
swzhxx <549909896@qq.com>
2022-02-16 20:08:22 +0800

    feat(self-parser): left right

--- self-parser/src/parser.rs
+++ self-parser/src/parser.rs
@@ -71,6 +71,22 @@ where
             .map(|(next_input, result)| (next_input, map_fn(result)))
     }
 }
+
+fn left<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R1>
+where
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
+{
+    map(pair(parser1, parser2), |(left, _right)| left)
+}
+
+fn right<'a, P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Parser<'a, R2>
+where
+    P1: Parser<'a, R1>,
+    P2: Parser<'a, R2>,
+{
+    map(pair(parser1, parser2), |(_left, right)| right)
+}
 #[cfg(test)]
 mod test {
     // use core::num::dec2flt::parse;
