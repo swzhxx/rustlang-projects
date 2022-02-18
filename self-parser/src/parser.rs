6deba03946cec752389ab7d99fce8115a883ecc4
swzhxx <549909896@qq.com>
2022-02-18 20:31:44 +0800

    feat(self parser): attribute parser

--- self-parser/src/parser.rs
+++ self-parser/src/parser.rs
@@ -174,6 +174,14 @@ fn quoted_string<'a>() -> impl Parser<'a, String> {
         |chars| chars.into_iter().collect(),
     )
 }
+
+fn attribute_pair<'a>() -> impl Parser<'a, (String, String)> {
+    pair(identifier, right(match_literal("="), quoted_string()))
+}
+
+fn attributes<'a>() -> impl Parser<'a, Vec<(String, String)>> {
+    zero_or_more(right(space1(), attribute_pair()))
+}
 #[cfg(test)]
 mod test {
     // use core::num::dec2flt::parse;
@@ -252,6 +260,23 @@ mod test {
 
     #[test]
     fn quoted_string_parser() {
-        assert_eq!(Ok(("", "Hello Joe!".to_string())), quoted_string().parse("\"Hello Joe!\""))
+        assert_eq!(
+            Ok(("", "Hello Joe!".to_string())),
+            quoted_string().parse("\"Hello Joe!\"")
+        )
+    }
+
+    #[test]
+    fn attribute_parser() {
+        assert_eq!(
+            Ok((
+                "",
+                vec![
+                    ("one".to_string(), "1".to_string()),
+                    ("two".to_string(), "2".to_string())
+                ]
+            )),
+            attributes().parse(" one=\"1\" two=\"2\"")
+        );
     }
 }
