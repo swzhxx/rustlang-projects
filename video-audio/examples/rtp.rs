364e792e04822c2bf95cbaf2bba2d7ccebbf1ff7
swzhxx <549909896@qq.com>
2022-02-28 20:24:06 +0800

    feat(rtp): rtp

---
+++ video-audio/examples/rtp.rs
@@ -0,0 +1,19 @@
+use std::io::BufReader;
+
+use anyhow::*;
+use bytes::{BufMut, BytesMut};
+use tokio::net::{TcpListener, UdpSocket};
+
+struct RTPHeader {
+    version: u8,
+    padding: bool,
+    extend: bool,
+    marker: bool,
+    payload: u8,
+    sn: u16,
+    time_stamp: u32,
+    ssrc: u32,
+    csrc_len: u32,
+}
+
+fn main() {}
