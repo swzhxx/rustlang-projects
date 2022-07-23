1fffca69c47ee145c1a0cb3382e16b9c88159925
swzhxx <549909896@qq.com>
2022-07-23 10:33:08 +0800

    feat(rusty ): map builder

--- platformer/src/main.rs
+++ platformer/src/main.rs
@@ -1,17 +1,45 @@
 use bevy::{prelude::App, prelude::*, window::WindowDescriptor};
+use bevy_rapier2d::{
+    plugin::{NoUserData, RapierPhysicsPlugin},
+    prelude::{
+        Ccd, Collider, ColliderMassProperties, Friction, GravityScale, Restitution, RigidBody,
+        Sensor, Sleeping, Velocity,
+    },
+    rapier::prelude::ColliderBuilder,
+};
 mod camera;
 //Player Component
 struct Player;
 
 fn spawn_player(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
-    commands.spawn_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.7, 0.7, 0.7),
-            custom_size: Some(Vec2::new(1.0, 1.0)),
+    // let rigid_body = RigidBodyBundle {
+    //     ..Default::default()
+    // };
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.7, 0.7, 0.7),
+                custom_size: Some(Vec2::new(1.0, 1.0)),
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        ..Default::default()
-    });
+        })
+        .insert(RigidBody::Dynamic)
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 5.0, 0.0)))
+        .insert(Velocity {
+            linvel: Vec2::new(1.0, 2.0),
+            angvel: 0.2,
+        })
+        .insert(GravityScale(0.5))
+        .insert(Sleeping::disabled())
+        .insert(Ccd::enabled())
+        .insert(Collider::cuboid(1.0, 2.0))
+        .insert(Sensor)
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(2.0, 0.0, 0.0)))
+        .insert(Friction::coefficient(0.7))
+        .insert(Restitution::coefficient(0.3))
+        .insert(ColliderMassProperties::Density(2.0));
 }
 
 fn setup(mut commands: Commands) {
@@ -31,5 +59,6 @@ fn main() {
         .add_startup_system(setup)
         .add_startup_stage("player_setup", SystemStage::single(spawn_player))
         .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .run()
 }
