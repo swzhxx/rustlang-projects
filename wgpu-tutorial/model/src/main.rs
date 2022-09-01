0e08784314fc13c8a3a50df9ba4b3107fe7de6c3
swzhxx <549909896@qq.com>
2022-09-01 20:36:35 +0800

    feat(model ): wgpu 加载模型

---
+++ wgpu-tutorial/model/src/main.rs
@@ -0,0 +1,16 @@
+use std::iter;
+
+use cgmath::prelude::*;
+use wgpu::util::DeviceExt;
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
+mod model;
+mod texture;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ wgpu-tutorial/model/src/model.rs
@@ -0,0 +1,20 @@
+use crate::texture;
+use std::ops::Range;
+
+pub trait Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a>;
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct ModelVertex {
+    position: [f32; 3],
+    tex_coords: [f32; 2],
+    normal: [f32; 3],
+}
+
+impl Vertex for ModelVertex {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        todo!()
+    }
+}
